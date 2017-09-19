4a95186bb2fba87f1e1767aed192a0a40795014e
VDABHEVERLEE\marc.de.jonge <marc.de.jonge1@telenet.be>
2017-09-19 09:57:06 +0200

    Path van Tekstbestand Eiland.txt aangepast in variabele bestandsnaam in Main

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -15,7 +15,7 @@ public class Main {
      */
     public static void main(String[] args) {
         
-        String bestandsnaam = "H:/Eiland.txt";
+        String bestandsnaam = "C:/Users/marc.de.jonge/Documents/NetBeansProjects/TestDesignPatterns/Eiland.txt";
         Eiland eiland = Eiland.getInstance();
         List<Inwoner> inwoners = new ArrayList<>();
         
